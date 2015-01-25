094d24d5455f7df50207c68f5bb79226c4af3a90
bulsatar <bulsatar@gmail.com>
2015-01-25 13:43:58 -0500

    test success?

--- gdx-game-android/src/com/mycompany/mygame/MainActivity.java
+++ gdx-game-android/src/com/mycompany/mygame/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mycompany.mygame.MyGdxGame;
+
 
 public class MainActivity extends AndroidApplication {
     @Override
